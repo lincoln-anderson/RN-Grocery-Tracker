4ae408b48f8618b9d161da9c54bd1b9136e48706
lincoln anderson <lincolnanderson@me.com>
2021-06-29 15:54:46 -0500

    set up environment

--- App.js
+++ App.js
@@ -1,66 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import 'react-native-gesture-handler';
-import { createStackNavigator } from '@react-navigation/stack';
-
-const Stack = createStackNavigator();
+import { Spacer, VStack, HStack, Text, Button } from 'swiftui-react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: 'Grocery Tracker' }}
-        />
-        <Stack.Screen name="Groceries" component={ProfileScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
-
-const HomeScreen = ({ navigation }) => {
-  return (
-    <Button
-      title="Mark food as Eaten"
-      onPress={() =>
-        navigation.navigate('Groceries', { name: 'Jane' })
-      }
-    />
-  );
-};
-const ProfileScreen = ({ navigation, route }) => {
-  return <Text></Text>;
-};
-
-const MyTitle = () => {
-  return(
-    <Text style={styles.welcome}>Hello</Text>
+    <View style={styles.container}>
+      <VStack
+      aligment='leading'
+      cornerRadius={20}
+      >
+        <HStack
+        aligment='leading'
+        cornerRadius={20}
+        >
+          <Spacer/>
+          <Text font='title'>Some cool text</Text>
+          <Spacer/>
+          <Text font='title2'>more text</Text>
+          <Spacer/>
+        </HStack>
+      </VStack>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
     alignItems: 'center',
-    backgroundColor: '#2A2734',
-  },
-  welcome: {
-    fontSize: 50,
-    textAlign: 'center',
-    margin: 10,
-    color: 'white',
-    borderWidth: 3,
-    borderColor: '#B0B0B0'
-  },
-  instructions: {
-    fontSize: 30,
-    textAlign: 'center',
-    color: '#B0B0B0',
-    marginBottom: 5,
+    justifyContent: 'center',
   },
 });
