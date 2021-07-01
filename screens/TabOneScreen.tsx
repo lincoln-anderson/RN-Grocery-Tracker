9ea7e078963ec1f32537a72cf85820ff1fcc90ad
lincoln anderson <lincolnanderson@me.com>
2021-07-01 11:10:36 -0500

    moRe home screen ui work. working on the grocery item object

---
+++ screens/TabOneScreen.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import GroceryItem from '../components/GroceryItem';
+import { Text, View } from '../components/Themed';
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      <GroceryItem name="Lincoln" date={ Date().getMonth() }/>
+      <Text style={styles.title}>lets see what groceries are about to go bad</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
