35bd777dc4fef8087044b2b7357f554c49cf71a7
spaghoo <lincolnanderson@me.com>
2021-07-10 14:42:28 -0500

    pushed views

---
+++ views/GroceryItemView.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import styles from '../../RN-Grocery-Tracker/styles/styles'
+
+class GroceryItem extends React.Component {
+  render() {
+    return <h1>Hello</h1>
+  }
+}
+
+export default GroceryItem;
