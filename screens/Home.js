dd7786be6cd652167807165e8f42884f5bbaea3a
spaghoo <lincolnanderson@me.com>
2021-07-10 13:44:30 -0500

    all files

---
+++ screens/Home.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import styles from '../../RN-Grocery-Tracker/styles/styles'
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Button
+        title="Go to Jane's profile"
+        onPress={() =>
+          navigation.navigate('Profile', { name: 'Jane' })
+        }
+      />
+      <View style={styles.space} />
+      <Button
+        title="Go to Test"
+        onPress={() =>
+          navigation.navigate('Test', { name: 'test' })
+        }
+      />
+    </View>
+  );
+};
+
+export default HomeScreen;
---
+++ screens/Subtest.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+const SubtestScreen = ({ navigation, route }) => {
+  return <Text>This is {route.params.name}'s profile</Text>;
+};
+
+
+export default SubtestScreen;
