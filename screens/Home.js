e64a49b2a02b624ecb98afffe97ec9997ea7540a
lincoln anderson <lincolnanderson@lincolns-MBP.attlocal.net>
2021-07-12 12:19:13 -0500

    passing props works correctly now

--- screens/Home.js
+++ screens/Home.js
@@ -9,22 +9,44 @@ import GroceryItemView from '../../RN-Grocery-Tracker/views/GroceryItemView'
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Button
-        title="Go to lincolns's profile"
-        onPress={() =>
-          navigation.navigate('Profile', { name: 'Jane' })
-        }
-      />
-      <GroceryItemView name="chicken"/>
+      <GroceryItemView name='steak' expDate={ getCurrentDate() } purchasedDate={ getPurchasedDate() } />
       <View style={styles.spacer} />
-      <Button
-        title="Go to Test"
-        onPress={() =>
-          navigation.navigate('Test', { name: 'test' })
-        }
-      />
+      <GroceryItemView name='chicken' expDate={ getCurrentDate() } purchasedDate={ getPurchasedDate() } />
     </View>
   );
 };
+
+
+//for testing
+
+const getPurchasedDate=()=>{
+
+    var date = new Date().getDate();
+    var month = new Date().getMonth() + 1;
+    var year = new Date().getFullYear();
+
+    //Alert.alert(date + '-' + month + '-' + year);
+    // You can turn it in to your desired format
+
+    console.log(date + '-' + month + '-' + year)
+    return date + '-' + month + '-' + year;//format: dd-mm-yyyy;
+
+
+}
+
+const getCurrentDate=()=>{
+
+    var date = new Date().getDate();
+    var month = new Date().getMonth() + 1;
+    var year = new Date().getFullYear();
+
+    //Alert.alert(date + '-' + month + '-' + year);
+    // You can turn it in to your desired format
+
+    console.log((date - 8) + '-' + month + '-' + year)
+    return (date - 8) + '-' + month + '-' + year;//format: dd-mm-yyyy;
+
+
+}
 
 export default HomeScreen;
--- views/GroceryItemView.js
+++ views/GroceryItemView.js
@@ -5,60 +5,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import styles from '../../RN-Grocery-Tracker/styles/styles'
 
-class GroceryItem extends React.Component {
-
-  state = {
-    name: 'steak',
-    expDate: getCurrentDate(),
-    purchasedDate: getPurchasedDate()
-  }
-
-  render() {
+function GroceryItem(props) {
     return(
       <View>
-        <Text>{ this.state.name }</Text>
+        <Text>{ props.name }</Text>
 
-        <Text>Purchased on: { this.state.expDate } </Text>
+        <Text>Purchased on: { props.expDate } </Text>
 
-        <Text>Expires on: { this.state.purchasedDate }</Text>
-
-
+        <Text>Expires on: { props.purchasedDate }</Text>
 
       </View>
     )
-  }
-}
+};
 
-//for testing
-
-const getCurrentDate=()=>{
-
-    var date = new Date().getDate();
-    var month = new Date().getMonth() + 1;
-    var year = new Date().getFullYear();
-
-    //Alert.alert(date + '-' + month + '-' + year);
-    // You can turn it in to your desired format
-
-    console.log(date + '-' + month + '-' + year)
-    return date + '-' + month + '-' + year;//format: dd-mm-yyyy;
-
-
-}
-
-const getPurchasedDate=()=>{
-
-    var date = new Date().getDate();
-    var month = new Date().getMonth() + 1;
-    var year = new Date().getFullYear();
-
-    //Alert.alert(date + '-' + month + '-' + year);
-    // You can turn it in to your desired format
-
-    console.log((date - 8) + '-' + month + '-' + year)
-    return (date - 8) + '-' + month + '-' + year;//format: dd-mm-yyyy;
-
-
-}
 
 export default GroceryItem;
